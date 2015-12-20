2fb0d9c205943efe2bf4ade3cd67de6fe7a482d8
andela-ydaniju <yusuf.daniju@andela.com>
2015-12-20 10:29:46 +0100

    create user controller action 'create'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def signup
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+    else
+      render "signup"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: "generals#index"
   get "users/signup", to: "users#signup", as: "signup"
   get "users/signin", to: "users#signin", as: "signin"
+  resources :users
 end
