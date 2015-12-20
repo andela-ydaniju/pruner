6394343dee63c40cac3c827da4768c37bc0891a8
andela-ydaniju <yusuf.daniju@andela.com>
2015-12-20 06:43:34 +0100

    user model up and running

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151219222557) do
+ActiveRecord::Schema.define(version: 20151220050725) do
 
   create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,37 @@
 require "rails_helper"
+
 RSpec.describe User, type: :model do
+  let(:user) do
+    User.new(
+      username: "andela-ydaniju",
+      email: "yusuf.daniju@andela.com"
+    )
+  end
+
+  context "when initialized" do
+    it "is not valid when username is more than fifty characters" do
+      user.username = "me" * 50
+      expect(user.valid?).to be false
+    end
+
+    it "is not valid when email is more than thirty characters" do
+      user.email = "me" * 30 + "@yahoo.com"
+      expect(user.valid?).to be false
+    end
+
+    it "is not valid when any field is not entered" do
+      user.username = nil
+      expect(user.valid?).to be false
+    end
+
+    it "is not valid when any field is not entered" do
+      user.email = nil
+      expect(user.valid?).to be false
+    end
+
+    it "has email which is case insensitive" do
+      user.email = "AJ@a.CoM"
+      expect(user.valid?).to be true
+    end
+  end
 end
