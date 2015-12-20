43f4282ea54f523e836c8ecaa429f7bc433ca798
andela-ydaniju <yusuf.daniju@andela.com>
2015-12-20 17:08:46 +0100

    Finish user authentication
    Why?
    
    User need to be able to signin when then when they get to the website
    
    How?
    
    Implement sessions
    
    https://www.pivotaltracker.com/story/show/110465740
    Finishes #110465740

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: "generals#index"
-  get "users/signup", to: "users#signup", as: "signup"
-  get "users/signin", to: "users#signin", as: "signin"
+  get "signup", to: "users#signup", as: "signup"
+  get "signin", to: "users#signin", as: "signin"
+  get "login", to: "sessions#new", as: "login"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy", as: "logout"
   resources :users
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ abort("The Rails environment is running in production mode!") if
 require "spec_helper"
 require "rspec/rails"
 require "capybara/rails"
+require "factory_girl_rails"
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -56,4 +57,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include FactoryGirl::Syntax::Methods
 end
