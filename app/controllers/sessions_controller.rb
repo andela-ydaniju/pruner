bafcccc797b06317858e27ad7ab230110d08f421
andela-ydaniju <yusuf.daniju@andela.com>
2016-01-15 11:37:37 +0100

    create move some sessions methods to service folder

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,4 +20,11 @@ class SessionsController < ApplicationController
     sign_out if signed_in?
     redirect_to root_url
   end
+
+  private
+
+  def current_user
+    user_id = session[:user_id]
+    @current_user ||= User.find_by(id: user_id)
+  end
 end
