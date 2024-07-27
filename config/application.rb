9bb31f1b17700029bbbdeb63a16d2f5be4462f45
ydaniju <yusuf.daniju@doctolib.com>
2024-07-27 12:37:34 +0200

    fix some more tests

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module Pruner
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales',
     # '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.load_defaults 7.0
     config.autoload_paths += %W(#{config.root}/app)
   end
 end
