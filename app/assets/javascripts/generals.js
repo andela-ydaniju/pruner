32b8d2a3e0d5158b1c186234c2246ada2e834f98
ydaniju <yusuf.daniju@doctolib.com>
2024-08-02 22:39:03 +0200

    remove jquery and zero clipboard

--- app/assets/javascripts/generals.js
+++ app/assets/javascripts/generals.js
@@ -1,10 +1,13 @@
 //= require materialize
 
-$(document).ready(function () {
-  var clip = new ZeroClipboard($(".clip_button"));
-  $(".clip_button").click(function () {
-		clip = new ZeroClipboard($(".clip_button"));
+(function () {
+  const clipButton = document.getElementById("clip_button");
 
-    M.toast({ html: 'Link copied to Clipboard', displayLength: 3000, classes: 'blue white-text squared' });
+  clipButton?.addEventListener("click", () => {
+    M.toast({
+      html: "Link copied to Clipboard",
+      displayLength: 3000,
+      classes: "blue white-text squared",
+    });
   });
-});
+})();
